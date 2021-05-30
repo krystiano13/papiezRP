e085a59aa43f4d8b349b454c32e47ed8e7ad128c
krystiano13 <krystiano13@interklasa.pl>
2021-05-30 21:40:59 +0200

    elo elo 320

---
+++ views/scripts/views.js
@@ -0,0 +1,156 @@
+const array = [
+  {
+    statusName: 's1',
+    title: 'Polska i Litwa po unii w Krewie',
+    text: 'W 1385roku Polska i Litwa podpisały układ w Krewie. Od tego czasu oba kraje pozostawały w unii personalnej – czyli miały wspólnego władcę. Na tronie polskim i litewskim zasiadali władcy wywodzący się z dynastii Jagiellonów. Oba państwa zachowały odrębne urzędy i instytucje państwowe. Szlachta litewska z zazdrością spoglądała na przywileje szlachty polskiej. Jednak najmożniejsi panowie litewscy niechętni byli ściślejszej unii, ponieważ obawiali się utraty pozycji na Litwie. Oni rządzili księstwem, gdy król przebywał w Polsce.',
+    imgUrl: 'im/k1.jpg',
+  },
+  {
+    statusName: 's2',
+    title: 'Unia Lubelska',
+    text: 'Król Zygmunt August obawiał się, że po jego bezpotomnej śmierci unia polsko‑litewska przestanie istnieć. Dążył więc do zacieśnienia związku obu państw, by był on trwalszy. W tym celu w 1569 roku król zwołał do Lublina sejm walny. Przybyli na niego nie tylko polscy posłowie, zaproszono również przedstawicieli Litwy. Wszyscy razem mieli podjąć decyzję o podpisaniu unii ściślej łączącej Królestwo Polskie i Wielkie Księstwo Litewskie. Po długich dyskusjach obie strony zgodziły się na podpisanie aktu unii lubelskiej. Z dwóch krajów, Polski i Litwy, utworzono jedno nowe państwo, które nazwano Rzeczpospolitą Obojga Narodów.',
+    imgUrl: 'im/k2.png',
+  },
+  {
+    statusName: 's3',
+    title: 'Sejm rozbiorowy w 1773 roku',
+    text: 'Kraje zaborcze domagały się od Polaków, aby zatwierdzili rozbiór i sami zrezygnowali z terenów zagarniętych przez sąsiadów. W tym celu Rosjanie nakazali królowi polskiemu zwołanie w 1773 roku sejmu rozbiorowego. Niemal wszyscy posłowie zostali przekupieni przez rosyjskiego ambasadora, dlatego podczas obrad prawie nikt nie sprzeciwił się tak jawnej niesprawiedliwości. Jednym z nielicznych protestujących był Tadeusz Rejtan, ale jego protest został zignorowany.',
+    imgUrl: 'im/k8.jpg',
+  },
+  {
+    statusName: 's4',
+    title: 'Drugi rozbiór Polski',
+    text: 'Po pierwszym rozbiorze wśród Polaków i Litwinów rosła świadomość konieczności przeprowadzenia reform. Zwołany w 1788 roku Sejm Wielki miał na trwałe zmienić cały kraj. Po uchwaleniu w 1791 roku Konstytucji 3 maja Katarzyna II zdała sobie sprawę, że zreformowana Rzeczpospolita może wkrótce stać się silnym i samodzielnym krajem. Tymczasem caryca wcale nie miała zamiaru rezygnować z podporządkowania sobie sąsiada. Dlatego niemal natychmiast po uchwaleniu Konstytucji rozpoczęła wojnę z państwem polsko‑litewskim, zakończoną kapitulacją Stanisława Augusta Poniatowskiego. Wkrótce po zwycięstwie wojsk carskich rozpoczęto prusko‑rosyjskie rozmowy na temat kolejnego rozbioru. Drugi rozbiór ziem Rzeczypospolitej, tym razem bez udziału Austrii, przeprowadzony został w 1793 roku. Podobnie jak po pierwszym rozbiorze, zaborcy nakazali polskiemu sejmowi zaakceptować zmiany granic. Uczynił to sejm zwołany w 1793 roku. Był on jednocześnie ostatnim sejmem Rzeczypospolitej szlacheckiej.',
+    imgUrl: 'im/k9.png',
+  },
+  {
+    statusName: 's5',
+    title: 'Rzeczpospolita przestaje istnieć',
+    text: 'Mieszkańcy Rzeczypospolitej nie chcieli się pogodzić z utratą większości ziem. Potężny niegdyś kraj został okrojony do niewielkiego państewka, na którego terenie stacjonowały wojska zaborcze. W 1794 roku Polacy pod wodzą Tadeusza Kościuszki jeszcze raz porwali się do walki. Mimo bohaterskiej postawy części szlachty, mieszczan, a nawet chłopów, powstanie zakończyło się klęską. Los Rzeczypospolitej został przesądzony. Po długich targach trzy kraje zaborcze postanowiły w 1795 roku dokonać trzeciego, ostatniego rozbioru. Rzeczpospolita Obojga Narodów przestała istnieć. Król Stanisław August Poniatowski abdykował i został wywieziony do Rosji, gdzie wkrótce zmarł. Dla wielu obywateli Rzeczypospolitej rozbiory były wielką tragedią. Dlatego w następnych latach podejmować będą działania w celu odbudowania niepodległego państwa polskiego. Uda się to dopiero po 123 latach.',
+    imgUrl: 'im/k10.png',
+  },
+  {
+    statusName: 's6',
+    title: 'Postanowienia unii lubelskiej',
+    text: 'Na czele Rzeczypospolitej Obojga Narodów miał stać jeden wspólny władca, powoływany przez Polaków i Litwinów. Król Polski był jednocześnie Wielkim Księciem Litewskim. Oba kraje miały mieć też wspólny sejm, czyli najwyższy organ władzy. Posłów na sejm wybierały sejmiki w Koronie i na Litwie. Wspólnie miała też być prowadzona polityka zagraniczna. Wprowadzono wspólną monetę. Litwa i Polska zachowały wiele odrębności. Przede wszystkim posiadały osobne armie, utrzymywane przez osobne skarby. Do skarbu polskiego (zwanego koronnym) wpływały podatki zbierane w Polsce, do skarbu litewskiego – pieniądze z podatków litewskich. Pozostawiono również osobne urzędy centralne, czyli w skład senatu wchodzili urzędnicy koronni i litewscy.',
+    imgUrl: 'im/k3.jpg',
+  },
+  {
+    statusName: 's7',
+    title: 'Rzeczpospolita Obojga Narodów',
+    text: 'Unia podpisana w Lublinie w 1569 roku doprowadziła do utworzenia Rzeczypospolitej Obojga Narodów. W wyniku połączenia Królestwa Polskiego i Wielkiego Księstwa Litewskiego powstał jeden z największych terytorialnie krajów w Europie. W państwie tym mieszkało wiele narodów (między innymi: Polacy, Litwini, Rusini, Kozacy, Żydzi) i wyznawano różne religie. Sprzyjało to rozwojowi kultury, ale rodziło też wiele problemów. Unia była niespotykanym wówczas przykładem, że dwa kraje mogą się porozumieć i zjednoczyć w pokojowy sposób. Przetrwała ponad dwieście lat i na trwałe wpisała się do historii obu państw.',
+    imgUrl: 'im/k4.jpg',
+  },
+  {
+    statusName: 's8',
+    title: 'Sąsiedzi Rzeczypospolitej w XVIII wieku',
+    text: 'W XVIII wieku Rzeczpospolita otoczona była przez trzech potężnych sąsiadów. Od wschodu graniczyła z Cesarstwem Rosyjskim, rządzonym przez Katarzynę II . Na południu znajdowało się rosnące w siłę Cesarstwo Austriackie, w którym panowała cesarzowa Maria Teresa i jej syn Józef II. Od zachodu i północy państwo polsko‑litewskie graniczyło z Prusami. Kraj ten był o wiele mniejszy od Rzeczypospolitej, ale miał olbrzymią armię. Jego władca Fryderyk II był najbardziej zainteresowany zagarnięciem części ziem polskich. Chciał w ten sposób powiększyć swój niewielki kraj i dzięki temu jeszcze bardziej rozbudować armię. Jedynym sąsiadem, który wówczas nie zagrażał polskim granicom, była Turcja.',
+    imgUrl: 'im/k5.jpg',
+  },
+  {
+    statusName: 's9',
+    title: 'Kryzys Rzeczypospolitej w XVII i XVIII wieku',
+    text: 'W XVII wieku Rzeczpospolita prowadziła liczne wojny. Ponieważ większość z nich toczyła się na jej terenie, doprowadziło to do ogromnych zniszczeń wielu regionów. W ciągu kilkunastu lat ludność państwa w wyniku wojen, chorób i głodu zmniejszyła się o 1/3. Wiele miast było w ruinie, chłopi opuścili spalone wsie, a nieuprawiana ziemia leżała odłogiem. Co więcej, wojny były bardzo kosztowne, ponieważ armię należało utrzymać. Zrujnowanej Polski nie było stać na tak wielkie wydatki, dlatego długi wobec wojska wciąż narastały. Jako że w kraju brakowało złota i srebra potrzebnego do produkcji pieniędzy, zaczęto zmniejszać ich zawartość w wybijanych monetach. To powodowało jednak, że miały one mniejszą wartość. Nowe monety były ',
+    imgUrl: 'im/k6.jpg',
+  },
+  {
+    statusName: 's10',
+    title: 'Pierwszy rozbiór Polski 1772 roku',
+    text: 'Stanisław August Poniatowski został w 1764 roku władcą Rzeczypospolitej dzięki poparciu Rosji. Cesarzowa Katarzyna II sądziła, że nowy król będzie jej zupełnie uległy. Jednak Poniatowski od początku rządów starał się wzmacniać swoje państwo. Nie podobało się to Katarzynie II, która przy pomocy swojego ambasadora, rezydującego w Warszawie, podburzała szlachtę przeciw proponowanym przez nowego króla reformom. W kraju na stałe przebywały wojska rosyjskie, którym nie mogła się przeciwstawić niewielka i źle wyszkolona armia Rzeczypospolitej.',
+    imgUrl: 'im/k7.png',
+  },
+];
+
+let status = 's1';
+let statusObj = array[1];
+
+const b1 = document.querySelector('#b1');
+const b2 = document.querySelector('#b2');
+const b3 = document.querySelector('#b3');
+const b4 = document.querySelector('#b4');
+const b5 = document.querySelector('#b5');
+const b6 = document.querySelector('#b6');
+const b7 = document.querySelector('#b7');
+const b8 = document.querySelector('#b8');
+const b9 = document.querySelector('#b9');
+const b10 = document.querySelector('#b10');
+
+const h1 = document.querySelector('h1');
+const article = document.querySelector('article');
+const img = document.querySelector('img');
+
+b1.addEventListener('click', () => {
+  status = 's1';
+  statusobj = array[0];
+  h1.innerText = array[0].title;
+  article.innerText = array[0].text;
+  img.src = array[0].imgUrl;
+});
+b2.addEventListener('click', () => {
+  status = 's2';
+  statusobj = array[1];
+  h1.innerText = array[1].title;
+  article.innerText = array[1].text;
+  img.src = array[1].imgUrl;
+});
+
+b3.addEventListener('click', () => {
+  status = 's3';
+  statusobj = array[2];
+  h1.innerText = array[2].title;
+  article.innerText = array[2].text;
+  img.src = array[2].imgUrl;
+});
+
+b4.addEventListener('click', () => {
+  status = 's4';
+  statusobj = array[3];
+  h1.innerText = array[3].title;
+  article.innerText = array[3].text;
+  img.src = array[3].imgUrl;
+});
+
+b5.addEventListener('click', () => {
+  status = 's5';
+  statusobj = array[4];
+  h1.innerText = array[4].title;
+  article.innerText = array[4].text;
+  img.src = array[4].imgUrl;
+});
+b6.addEventListener('click', () => {
+  status = 's6';
+  statusobj = array[5];
+  h1.innerText = array[5].title;
+  article.innerText = array[5].text;
+  img.src = array[5].imgUrl;
+});
+b7.addEventListener('click', () => {
+  status = 's7';
+  statusobj = array[6];
+  h1.innerText = array[6].title;
+  article.innerText = array[6].text;
+  img.src = array[6].imgUrl;
+});
+
+b8.addEventListener('click', () => {
+  status = 's8';
+  statusobj = array[7];
+  h1.innerText = array[7].title;
+  article.innerText = array[7].text;
+  img.src = array[7].imgUrl;
+});
+
+b9.addEventListener('click', () => {
+  status = 's9';
+  statusobj = array[8];
+  h1.innerText = array[8].title;
+  article.innerText = array[8].text;
+  img.src = array[8].imgUrl;
+});
+b10.addEventListener('click', () => {
+  status = 's10';
+  statusobj = array[9];
+  h1.innerText = array[9].title;
+  article.innerText = array[9].text;
+  img.src = array[9].imgUrl;
+});
